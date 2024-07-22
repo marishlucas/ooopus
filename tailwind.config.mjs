9223960253eb3dbe179a95eec7b4ef8b97c55cf7
lucas <lucas.m.maris@gmail.com>
2024-07-22 15:09:03 +0300

    [ feat ] - added Oswald to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import fluid, { extract } from "fluid-tailwind";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: {
+    files: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    extract,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      },
+    },
+  },
+  plugins: [fluid],
+};
