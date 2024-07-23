67e7ce8451b00c03bf1c94e42b7562f1e323a178
lucas <lucas.m.maris@gmail.com>
2024-07-23 22:52:14 +0300

    [ fix ] - removed smooth scrolling on mobile due to browser UI problems
    navbar on mobile takes part of the viewport, thus hiding some of the UI

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -22,9 +22,6 @@ class Animation {
     scroll = new LocomotiveScroll({
       el: document.querySelector("[data-scroll-container]"),
       smooth: true,
-      smartphone: {
-        smooth: true,
-      },
     });
     new ResizeObserver(() => scroll.update()).observe(
       document.querySelector("[data-scroll-container]"),
